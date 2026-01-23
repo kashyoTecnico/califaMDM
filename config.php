48884578c40ebdfc9e256b402db806e44c1f6073
kashyoTecnico <122602175+kashyoTecnico@users.noreply.github.com>
2026-01-22 17:36:47 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
+// =======================
 // CONFIGURACIÓN MDM
-define("MDM_SECRET", "CALIFA_MDM_7429");
+// =======================
+
 define("MDM_TOKEN", "7429");
+define("MDM_SECRET", "CALIFA_MDM_7429");
+
+// archivo donde se guarda el comando
 define("CMD_FILE", __DIR__ . "/commands.json");
