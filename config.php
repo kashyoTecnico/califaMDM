0596395602f59cabc2c1cd1e1fd31e05b4e217fb
kashyoTecnico <122602175+kashyoTecnico@users.noreply.github.com>
2026-01-28 02:23:12 -0800

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,11 @@ define("MDM_SECRET", "CALIFA_MDM_7429");
 define("CMD_DIR", __DIR__."/cmd");
 define("STATUS_DIR", __DIR__."/status");
 define("ACK_DIR", __DIR__."/ack");
+define("CMD_FILE", __DIR__ . "/commands.json");
+
+// opcional: evitar errores silenciosos
+error_reporting(E_ALL);
+ini_set("display_errors", 0);
 
 @mkdir(CMD_DIR);
 @mkdir(STATUS_DIR);
