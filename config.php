09f7072312d297da54809edb7859e68788f5daa6
kashyoTecnico <122602175+kashyoTecnico@users.noreply.github.com>
2026-01-28 00:24:49 -0800

    Update config.php

--- config.php
+++ config.php
@@ -2,5 +2,10 @@
 define("MDM_TOKEN", "7429");
 define("MDM_SECRET", "CALIFA_MDM_7429");
 
-define("CMD_FILE", __DIR__."/command.json");
-define("STATUS_FILE", __DIR__."/status.json");
+define("CMD_DIR", __DIR__."/cmd");
+define("STATUS_DIR", __DIR__."/status");
+define("ACK_DIR", __DIR__."/ack");
+
+@mkdir(CMD_DIR);
+@mkdir(STATUS_DIR);
+@mkdir(ACK_DIR);
